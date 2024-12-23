3c06d99976e5fe4fb6884951e37f7267e95b85ec
megalodon <cryptid.megalodon@gmail.com>
2024-12-23 13:59:33 -0500

    Updates to spirit card and new fonts

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
-import { Stack, useRouter, usePathname } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { View, StyleSheet, ActivityIndicator } from 'react-native';
 import { AuthProvider, useAuth } from '@/contexts/AuthContext';
 import { SpiritProvider } from '@/contexts/SpiritContext';
@@ -7,7 +9,25 @@ import { TeamsProvider } from '@/contexts/TeamContext';
 import { BattleProvider } from '@/contexts/BattleContext';
 import { ParamProvider } from '@/contexts/ParamContext';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    'LibreBaskerville-Regular': require('@/assets/fonts/LibreBaskerville-Regular.ttf'),
+    'LibreBaskerville-Italic': require('@/assets/fonts/LibreBaskerville-Italic.ttf'),
+    'LibreBaskerville-Bold': require('@/assets/fonts/LibreBaskerville-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <AuthProvider>
       <SpiritProvider>
--- client/components/SpiritCard.tsx
+++ client/components/SpiritCard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Modal, View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { Modal, View, Text, Image, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
 import { Spirit } from '@/models/Spirit';
 
 interface SpiritCardProps {
@@ -36,15 +36,15 @@ const SpiritCard: React.FC<SpiritCardProps> = ({ visible, spiritData, onClose })
   return (
     <Modal visible={visible} transparent={true} animationType="slide">
       <View style={styles.modalContainer}>
-        <View style={styles.outerContainer}>
-          <View style={styles.innerContainer}>
-            <Image
-              source={typeImages[spiritData.primaryType]}
-              style={styles.typeIcon}
-            />
-            <Text style={styles.name}>{spiritData.name}</Text>
-
-            <View style={styles.imageContainer}>
+          <View style={styles.spiritCard}>
+            <View style={styles.cardHeader}>
+              <Text style={styles.spiritName}>{spiritData.name}</Text>
+              <Image
+                source={typeImages[spiritData.primaryType]}
+                style={styles.typeIcon}
+              />
+            </View>
+            <View style={styles.cardBody}>
               {/* Generated Image */}
               <Image
                 source={{
@@ -52,7 +52,7 @@ const SpiritCard: React.FC<SpiritCardProps> = ({ visible, spiritData, onClose })
                     ? spiritData.originalImageDownloadUrl
                     : spiritData.generatedImageDownloadUrl,
                 }}
-                style={styles.fullImage}
+                style={styles.mainImage}
               />
 
               {/* Overlay for Original Image */}
@@ -66,25 +66,43 @@ const SpiritCard: React.FC<SpiritCardProps> = ({ visible, spiritData, onClose })
                   style={styles.overlayImage}
                 />
               </TouchableOpacity>
-            </View>
 
-            <View style={styles.bannerContainer}>
-              <Text style={styles.bannerText}>
-                {truncateDescription(spiritData.description)} {'•'} HT: {spiritData.height}cm {'•'} WT: {spiritData.weight}kg
-              </Text>
+              <View style={styles.bannerContainer}>
+                <Text style={styles.bannerText}>
+                  Jelly Creature {'•'} HT: {spiritData.height}cm {'•'} WT: {spiritData.weight}kg
+                </Text>
+              </View>
             </View>
-
-            <TouchableOpacity onPress={onClose} style={styles.closeButton}>
-              <Text>Close</Text>
-            </TouchableOpacity>
+            <View style={styles.spiritMoves}>
+              <View style={styles.spiritMovesRow}>
+                <Text style={styles.spiritMovesTitle}>Punch</Text>
+                <Text style={styles.spiritMovesTitle}>Special</Text>
+              </View>
+              <View style={styles.spiritMovesRow}>
+                <Text style={styles.spiritMovesTitle}>Tag Team</Text>
+                <Text style={styles.spiritMovesTitle}>Growl</Text>
+              </View>
+            </View>
+            <Text style={styles.spiritDescription}>"{truncateDescription(spiritData.description)}" —Spiritologist</Text>
+            <View style={styles.cardFooter}>
+              <Text style={styles.footerText}>© 2024 Spirit Snap — Created: date</Text>
+            </View>
           </View>
-        </View>
+        <TouchableOpacity onPress={onClose} style={styles.closeButton}>
+          <Text>Close</Text>
+        </TouchableOpacity>
       </View>
     </Modal>
   );
 };
 
 export default SpiritCard;
+
+const { width, height } = Dimensions.get('window');
+// Determine the max size based on the screen size and aspect ratio
+const aspectRatio = 5 / 7;
+const cardWidth = Math.min(width, height * (7 / 5));
+const cardHeight = cardWidth / aspectRatio;
 
 const styles = StyleSheet.create({
   modalContainer: {
@@ -94,105 +112,120 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.8)',
     padding: 10,
   },
-  outerContainer: {
-    maxWidth: '100%', // Ensure it doesn't exceed the screen width
-    maxHeight: '97%', // Constrain height to avoid overlapping the top or bottom
-    padding: 15,
-    backgroundColor: 'orange',
-    borderRadius: 10, // Rounded edges for the outer container
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 5 },
-    shadowOpacity: 0.2,
-    shadowRadius: 10,
-    elevation: 5,
-  },
-  innerContainer: {
-    width: '100%',
-    maxHeight: '100%',
+  spiritCard: {
+    width: cardWidth,
+    height: cardHeight,
+    borderColor: 'orange',
+    borderRadius: 10,
+    borderWidth: cardWidth * .035,
     backgroundColor: '#d3d3d3', // Inner container background color
-    borderRadius: 0, // Square edges for the inner container
     alignItems: 'center',
     overflow: 'hidden',
     justifyContent: 'center',
   },
+  cardHeader: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  spiritName: {
+    fontSize: cardWidth * .045,
+    color: '#000',
+    textAlign: 'left',
+    alignSelf: 'stretch',
+    marginTop: cardWidth * .02,
+    marginLeft: cardWidth * .03,
+    fontFamily: 'LibreBaskerville-Bold',
+  },
   typeIcon: {
-    position: 'absolute',
-    top: 5,
-    right: 5,
-    width: 35,
-    height: 35,
+    width: cardWidth * .07,
+    height: cardWidth * .07,
+    margin: cardWidth * .012,
     borderRadius: 25, // Makes the image circular
-    borderWidth: 2,
-    borderColor: '#fff', // Optional border for better visibility
+    borderWidth: cardWidth * .002,
+    borderColor: '#fff',
+    backgroundColor: 'black',
     resizeMode: 'contain', // Ensures the entire image is visible
     overflow: 'hidden', // Ensures circular shape without cropping issues
   },
-  imageContainer: {
-    position: 'relative', // Allows absolute positioning for the overlay
-    width: '100%',
-    aspectRatio: 1,
-    marginBottom: 20,
-    justifyContent: 'center',
+  cardBody: {
+    position: 'relative', // allows absolute positioning for the overlay
+    flex: 1,
+    width: '90%',
     alignItems: 'center',
   },
-  fullImage: {
-    width: '90%', // Slightly smaller than the container
-    aspectRatio: 1, // Maintain square dimensions
-    resizeMode: 'cover',
-    backgroundColor: '#fff', // Optional background for better contrast
-    borderRadius: 0,
-    borderWidth: 2,
-    borderColor: '#8B4000',
+  mainImage: {
+    flex: 1,
+    alignSelf: 'stretch',
+    borderWidth: cardWidth * .004,
+    borderColor: 'black',
   },
   overlayContainer: {
     position: 'absolute',
-    bottom: 5, // Spacing from the bottom
-    right: 5, // Spacing from the right
-    width: 80,
-    height: 80,
+    bottom: cardWidth * .01,
+    right: cardWidth * .01,
+    width: cardWidth * .16,
+    height: cardWidth * .16,
     borderRadius: 10,
     overflow: 'hidden',
-    borderWidth: 2, // Thin border
-    borderColor: '#000', // Black border color
+    borderWidth: cardWidth * .004,
+    borderColor: '#000',
   },
   overlayImage: {
     width: '100%',
     height: '100%',
     resizeMode: 'cover',
   },
-  name: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#000',
-    textAlign: 'left',
-    alignSelf: 'stretch',
-    marginBottom: 5,
-    marginTop: 10,
-    marginLeft: 10,
-  },
   bannerContainer: {
     width: '100%',
-    backgroundColor: '#CC5500', // Banner background color
-    padding: 10,
-    borderRadius: 0,
-    marginTop: -25, // Slight overlap with the image
-    alignItems: 'center', // Center the text horizontally
+    backgroundColor: '#CC5500',
+    padding: cardWidth * .01,
+    alignItems: 'center',
   },
   bannerText: {
-    fontSize: 14,
-    color: 'white', // Black text
+    fontSize: cardWidth * .025,
+    color: 'white',
     textAlign: 'left',
+    fontFamily: 'LibreBaskerville-Regular',
   },
-  smallImageContainer: {
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
+  spiritMoves: {
+    width: '100%',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: cardWidth * .02,
   },
-  smallImage: {
-    width: 80,
-    height: 80,
-    borderRadius: 10,
+  spiritMovesRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    marginBottom: cardWidth * .01,
+  },
+  spiritMovesTitle: {
+    fontSize: cardWidth * .035,
+    color: 'black',
+    textAlign: 'center',
+    fontFamily: 'LibreBaskerville-Regular',
+  },
+  cardFooter: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: cardWidth * .006,
+  },
+  spiritDescription: {
+    fontSize: cardWidth * .03,
+    color: 'black',
+    textAlign: 'center',
+    padding: cardWidth * .02,
+    fontFamily: 'LibreBaskerville-Italic',
+  },
+  footerText: {
+    fontSize: cardWidth * .02,
+    color: 'black',
   },
   closeButton: {
     padding: 10,
